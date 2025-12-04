8e0d172e146a582f3c94f496b6a5675f788e1ae0
mateiavram <mateiavram2004@gmail.com>
2025-12-04 18:51:22 +0200

    optimizare baze de date

--- BACK_END/server.js
+++ BACK_END/server.js
@@ -13,16 +13,16 @@ const Conference = require("./models/conference");
 Author.hasMany(Article, { foreignKey: "authorId" });
 Article.belongsTo(Author, { foreignKey: "authorId" });
 Conference.hasMany(Article, { foreignKey: "conferenceId" });
-Article.belongsTo(Conference, { foreignKey: "conferenceId"} );
+Article.belongsTo(Conference, { foreignKey: "conferenceId" });
 Reviewer.hasMany(Review, { foreignKey: "reviewerId" });
 Review.belongsTo(Reviewer, { foreignKey: "reviewerId" });
 Article.hasMany(Review, { foreignKey: "articleId" });
 Review.belongsTo(Article, { foreignKey: "articleId" });
 
 app.use(
-  express.urlencoded({
-    extended: true,
-  })
+    express.urlencoded({
+        extended: true,
+    })
 );
 app.use(express.json());
 
@@ -30,11 +30,11 @@ app.use((err, req, res, next) => {
     res.status(500).json({ message: "500 - Server Error" });
 });
 
-app.get("/create", async(req, res) => {
+app.get("/create", async (req, res) => {
     await sequelize.sync({
         force: true
     });
-    res.json({message: "Database created!"});
+    res.json({ message: "Database created!" });
 });
 
 
@@ -45,7 +45,7 @@ app.get("/authors", async (req, res, next) => {
     try {
         const authors = await Author.findAll();
         res.status(200).json(authors);
-    } catch(err) {
+    } catch (err) {
         next(err);
     }
 });
@@ -54,34 +54,34 @@ app.get("/reviewers", async (req, res, next) => {
     try {
         const reviewers = await Reviewer.findAll();
         res.status(200).json(reviewers);
-    } catch(err) {
+    } catch (err) {
         next(err);
     }
 });
 
 app.get("/articles", async (req, res, next) => {
     try {
-        const articles= await Article.findAll();
+        const articles = await Article.findAll();
         res.status(200).json(articles);
-    } catch(err) {
+    } catch (err) {
         next(err);
     }
 });
 
 app.get("/conferences", async (req, res, next) => {
     try {
-        const conf= await Conference.findAll();
+        const conf = await Conference.findAll();
         res.status(200).json(conf);
-    } catch(err) {
+    } catch (err) {
         next(err);
     }
 });
 
 app.get("/organisers", async (req, res, next) => {
     try {
-        const org= await Organiser.findAll();
+        const org = await Organiser.findAll();
         res.status(200).json(org);
-    } catch(err) {
+    } catch (err) {
         next(err);
     }
 });
@@ -89,9 +89,9 @@ app.get("/organisers", async (req, res, next) => {
 
 app.get("/reviews", async (req, res, next) => {
     try {
-        const rev= await Review.findAll();
+        const rev = await Review.findAll();
         res.status(200).json(rev);
-    } catch(err) {
+    } catch (err) {
         next(err);
     }
 });
@@ -105,51 +105,58 @@ app.get("/reviews", async (req, res, next) => {
 //Conference has a Organiser
 //Review has a reviewer
 
-app.get("/authors/:authorId/articles",async(req,res,next)=>{
-    try{
-        const author=Author.findByPk(req.params.authorId,
-            {include: [Article]})
-        if(!author){
-            res.status(404).json({message:"Nu am gasit autorul"})
+app.get("/authors/:authorId/articles", async (req, res, next) => {
+    try {
+        const author = await Author.findByPk(req.params.authorId,
+            {
+                include: [Article]
+
+            });
+        if (!author) {
+            res.status(404).json({ message: "Nu am gasit autorul" })
         }
-        else
-        res.status(202).json(author.articles);
+        else {
+            //const articles=author.articles;
+            res.status(202).json(author.articles);
+        }
+
     }
-    catch(err){
+    catch (err) {
         next(err);
     }
 });
 
-app.get("organisers/:orgId/conferences",async(req,res,next)=>{
 
-    try{
-        const org=Organiser.findByPk(req.params.orgId,{
-            include:[Conference]
+app.get("organisers/:orgId/conferences", async (req, res, next) => {
+
+    try {
+        const org = Organiser.findByPk(req.params.orgId, {
+            include: [Conference]
         })
-        if(!org){
-            res.status(404).json({message:"Nu exista organizator cu acest id"})
+        if (!org) {
+            res.status(404).json({ message: "Nu exista organizator cu acest id" })
         }
-        else{
+        else {
             res.status(202).json(org.conferences)
         }
     }
-    catch(err){
+    catch (err) {
         next(err);
     }
 });
 
 //---------------------------------------------------------
-app.get("/reviewers/:revId/reviews",async(req,res,next)=>{
-    try{
-        const rev=Review.findByPk(req.params.revId);
-        if(!rec){
-            res.status(404).json({message:"Nu am gasit reviewer"})
+app.get("/reviewers/:revId/reviews", async (req, res, next) => {
+    try {
+        const rev = Review.findByPk(req.params.revId);
+        if (!rec) {
+            res.status(404).json({ message: "Nu am gasit reviewer" })
         }
-        else{
+        else {
             res.status(202).json(rev.reviews);
         }
     }
-    catch(err){
+    catch (err) {
         next(err);
     }
 })
@@ -165,15 +172,15 @@ app.get("/reviewers/:revId/reviews",async(req,res,next)=>{
 app.delete("/authors/:idAuthor", async (req, res, next) => {
     try {
         const idAuthor = req.params.idAuthor;
-        const deletedCount = await Author.destroy({ where: {idAuthor} });
+        const deletedCount = await Author.destroy({ where: { idAuthor } });
 
-        if(deletedCount === 0) {
+        if (deletedCount === 0) {
             res.status(404).json({ message: "Author not found" });
         }
         else {
             res.status(200).json({ message: "Author deleted successfully" });
         }
-    } catch(err) {
+    } catch (err) {
         next(err);
     }
 });
@@ -181,15 +188,15 @@ app.delete("/authors/:idAuthor", async (req, res, next) => {
 app.delete("/reviewers/:idR", async (req, res, next) => {
     try {
         const idAuthor = req.params.idAuthor;
-        const deletedCount = await Reviewer.destroy({ where: {idR} });
+        const deletedCount = await Reviewer.destroy({ where: { idR } });
 
-        if(deletedCount === 0) {
+        if (deletedCount === 0) {
             res.status(404).json({ message: "Author not found" });
         }
         else {
             res.status(200).json({ message: "Author deleted successfully" });
         }
-    } catch(err) {
+    } catch (err) {
         next(err);
     }
 });
@@ -197,15 +204,15 @@ app.delete("/reviewers/:idR", async (req, res, next) => {
 app.delete("/organisers/:idO", async (req, res, next) => {
     try {
         const idAuthor = req.params.idAuthor;
-        const deletedCount = await Organiser.destroy({ where: {idO} });
+        const deletedCount = await Organiser.destroy({ where: { idO } });
 
-        if(deletedCount === 0) {
+        if (deletedCount === 0) {
             res.status(404).json({ message: "Author not found" });
         }
         else {
             res.status(200).json({ message: "Author deleted successfully" });
         }
-    } catch(err) {
+    } catch (err) {
         next(err);
     }
 });
@@ -213,15 +220,15 @@ app.delete("/organisers/:idO", async (req, res, next) => {
 app.delete("/conferences/:idC", async (req, res, next) => {
     try {
         const idAuthor = req.params.idAuthor;
-        const deletedCount = await Reviewer.destroy({ where: {idC} });
+        const deletedCount = await Reviewer.destroy({ where: { idC } });
 
-        if(deletedCount === 0) {
+        if (deletedCount === 0) {
             res.status(404).json({ message: "Author not found" });
         }
         else {
             res.status(200).json({ message: "Author deleted successfully" });
         }
-    } catch(err) {
+    } catch (err) {
         next(err);
     }
 });
@@ -229,15 +236,15 @@ app.delete("/conferences/:idC", async (req, res, next) => {
 app.delete("/reviews/:idR", async (req, res, next) => {
     try {
         const idAuthor = req.params.idAuthor;
-        const deletedCount = await Review.destroy({ where: {idR} });
+        const deletedCount = await Review.destroy({ where: { idR } });
 
-        if(deletedCount === 0) {
+        if (deletedCount === 0) {
             res.status(404).json({ message: "Author not found" });
         }
         else {
             res.status(200).json({ message: "Author deleted successfully" });
         }
-    } catch(err) {
+    } catch (err) {
         next(err);
     }
 });
@@ -246,15 +253,15 @@ app.delete("/reviews/:idR", async (req, res, next) => {
 app.delete("/articles/:idA", async (req, res, next) => {
     try {
         const idAuthor = req.params.idAuthor;
-        const deletedCount = await Article.destroy({ where: {idA} });
+        const deletedCount = await Article.destroy({ where: { idA } });
 
-        if(deletedCount === 0) {
+        if (deletedCount === 0) {
             res.status(404).json({ message: "Author not found" });
         }
         else {
             res.status(200).json({ message: "Author deleted successfully" });
         }
-    } catch(err) {
+    } catch (err) {
         next(err);
     }
 });
@@ -269,7 +276,7 @@ app.post("/reviewer", async (req, res, next) => {
     try {
         const reviewer = await Reviewer.create(req.body);
         res.status(201).json(reviewer);
-    } catch(err) {
+    } catch (err) {
         next(err);
     }
 });
@@ -279,7 +286,7 @@ app.post("/conference", async (req, res, next) => {
     try {
         const conference = await Conference.create(req.body);
         res.status(201).json(conference);
-    } catch(err) {
+    } catch (err) {
         next(err);
     }
 });
@@ -290,49 +297,49 @@ app.post("/author", async (req, res, next) => {
     try {
         const author = await Author.create(req.body);
         res.status(201).json(author);
-    } catch(err) {
+    } catch (err) {
         next(err);
     }
 });
 
-app.post("/article",async(req,res,next)=>{
-    try{
-        const article=await Article.create(req.body);
+app.post("/article", async (req, res, next) => {
+    try {
+        const article = await Article.create(req.body);
         res.status(201).json(article);
         res.status(201).json({
-            message:"Articol creat",
-            
+            message: "Articol creat",
+
         });
     }
-    catch(err){
+    catch (err) {
         next(err);
     }
 })
 
-app.post("/review",async(req,res,next)=>{
-    try{
-        const review=await Review.create(req.body);
+app.post("/review", async (req, res, next) => {
+    try {
+        const review = await Review.create(req.body);
         res.status(201).json({
-            message:"Review creat",
-            
+            message: "Review creat",
+
         });
 
     }
-    catch(error){
+    catch (error) {
         next(error);
     }
 })
 
-app.post("/organiser",async(req,res,next)=>{
-    try{
-        const org=await Organiser.create(req.body);
+app.post("/organiser", async (req, res, next) => {
+    try {
+        const org = await Organiser.create(req.body);
         res.status(201).json({
-            message:"Organiser creat",
-            data:org
+            message: "Organiser creat",
+            data: org
         });
 
     }
-    catch(error){
+    catch (error) {
         next(error);
     }
 })
